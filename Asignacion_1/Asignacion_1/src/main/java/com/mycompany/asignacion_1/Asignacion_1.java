fc66b05be9c6dc4d505bdc35366fd853c22d9fc1
leykoss <56200866+leykoss@users.noreply.github.com>
2025-01-07 23:39:37 -0400

    Folder de Asignacion 1 creada

---
+++ Asignacion_1/Asignacion_1/src/main/java/com/mycompany/asignacion_1/Asignacion_1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.asignacion_1;
+
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author leykr
+ */
+public class Asignacion_1 {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainMenu m = new MainMenu();
+            }
+        });
+    }
+}
